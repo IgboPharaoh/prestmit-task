c1caa8588c8ef147cb9c55a9ff814580f7f23409
IgboPharaoh <solomoneze62@gmail.com>
2023-08-24 18:46:03 +0100

    Add Blog Section to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import BlogSection from './components/blog-section/BlogSection';
 import FeatureSection from './components/features-section/FeatureSection';
 import Hero from './components/hero-page/Hero';
 import ServiceSection from './components/service-section/ServiceSection';
@@ -12,6 +13,7 @@ function App() {
             <ServiceSection />
             <FeatureSection />
             <Testimonials />
+            <BlogSection />
         </div>
     );
 }
