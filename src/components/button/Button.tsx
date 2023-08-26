294f3518557ee34c93dd24e640cd308b63d1c19b
IgboPharaoh <solomoneze62@gmail.com>
2023-08-26 20:08:25 +0100

    responsive styling for button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -9,21 +9,58 @@ export interface ButtonProps {
     height?: string;
     padding?: string;
     fontSize?: string;
+    otherStyles?: React.CSSProperties;
 }
 
-const Button: React.FC<ButtonProps> = ({ name, backgroundColor, color, border, height = '40px', padding = '56px', fontSize = '20px' }) => {
+const getWindowSizes = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return { width, height };
+};
+
+const useWindowSizes = () => {
+    const [windowSize, setwindowSize] = React.useState({ width: 0, height: 0 });
+
+    React.useEffect(() => {
+        setwindowSize(getWindowSizes());
+        function resize() {
+            setwindowSize(getWindowSizes());
+        }
+
+        window.addEventListener('resize', resize);
+
+        return () => window.removeEventListener('resize', resize);
+    }, []);
+
+    return windowSize;
+};
+
+const Button: React.FC<ButtonProps> = ({
+    name,
+    backgroundColor,
+    color,
+    border,
+    height = '40px',
+    padding = '56px',
+    fontSize = '20px',
+    otherStyles,
+}) => {
+    const { width } = useWindowSizes();
+
+    const buttonPaddings = width <= 1260 ? `0px 24px` : width <= 900 ? `0px 24px` : width <= 600 ? `0px 20px` : `0px ${padding}`;
+    const buttonFontSizes = width <= 550 ? `16px` : fontSize;
+    const buttonHeight = width <= 550 ? `48px` : height;
+
     const buttonStyles: React.CSSProperties = {
         color: color,
         backgroundColor: backgroundColor,
         border: border ? border : 'transparent',
-        height: height,
-        borderRadius: '56px',
-        padding: `0px ${padding}`,
-        fontSize: fontSize ? fontSize : '',
+        height: buttonHeight,
+        padding: buttonPaddings,
+        fontSize: buttonFontSizes,
     };
 
     return (
-        <button style={buttonStyles} className={styles.buttonWrapper}>
+        <button style={{ ...buttonStyles, ...otherStyles }} className={styles.buttonWrapper}>
             {name}
         </button>
     );
