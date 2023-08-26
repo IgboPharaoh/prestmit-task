09364601c5421038cbe33556fdb53a901e8de9b3
IgboPharaoh <solomoneze62@gmail.com>
2023-08-26 20:06:40 +0100

    responsive styling for servcie section

--- src/components/service-section/ServiceSection.tsx
+++ src/components/service-section/ServiceSection.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import Wrapper from '../../Wrapper';
-import ServiceCard from './ServiceCard';
 import styles from './styles.module.css';
 import WalkingHuman from '../../assets/walking-human.webp';
 import SittingHuman from '../../assets/sitting-human.webp';
 import ShakingHuman from '../../assets/shaking-humans.webp';
+import { ReactComponent as ForwardIcon } from '../../assets/forward.svg';
+
+interface ServiceCardProps {
+    src: string;
+    title: string;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({ src, title }) => {
+    return (
+        <div className={styles.serviceCardWrapper}>
+            <div className={styles.serviceCardImageWrapper}>
+                <img src={src} alt='' className={styles.serviceCardImage} />
+            </div>
+            <p className={styles.serviceCardText}>{title}</p>
+            <section className={styles.serviceCardBtnWrapper}>
+                <div className={styles.serviceCardBtnWrapperMobile}>
+                    <p className={styles.serviceCardLink}>Expore page</p>
+                    <ForwardIcon />
+                </div>
+            </section>
+        </div>
+    );
+};
 
 const ServiceSection = () => {
     return (
